303e92dd820ec6237a04115de04b34253827f1f2
itsfadnis <nik.fadnis@gmail.com>
2018-03-16 09:19:26 +0530

    Add proper bootstrapping mechanism

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
     main: './src/index.js'
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    library: '@itsfadnis/jsonapi-client',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
     rules: [
